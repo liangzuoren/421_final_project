5e3351a635f45c830c517007cb8d56f4076d4462
liangzuoren <lzr1@rice.edu>
2016-11-17 11:46:12 -0600

    add PC time syncing

--- StepCounter/StepCounter.ino
+++ StepCounter/StepCounter.ino
@@ -2,6 +2,7 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <Time.h>
+#include <TimeLib.h>
 
 // Counts the number of steps that you are taken using the accelerometer
 
@@ -10,6 +11,9 @@ int steps;
 String val;
 boolean recording = false;
 boolean saveData = false;
+#define TIME_HEADER  'T'   // Header tag for serial time sync message
+#define TIME_REQUEST  'z'    // ASCII bell character requests a time sync message 
+boolean synced = false;
 
 void setup(void) {
   
@@ -19,6 +23,23 @@ void setup(void) {
 }
 
 void loop() {  
+  if(synced == false){
+     setSyncProvider(requestSync);  
+     if (Serial.available()) {
+        processSyncMessage();
+     }
+     if (timeStatus()!= timeNotSet) {
+        digitalClockDisplay();  
+     }
+     if (timeStatus() == timeSet) {
+        CircuitPlayground.setPixelColor(0, 0,   255,   0); // LED green if synced
+        synced = true;
+     } else {
+        CircuitPlayground.setPixelColor(0, 255, 0, 0);  // LED red if needs refresh
+     }
+     delay(500);
+  }
+  
   if(CircuitPlayground.rightButton()){
    steps = 0;
    recording = true; 
@@ -45,19 +66,17 @@ void loop() {
   }
 
   if(saveData == true){
-//    Serial.print(steps);
-//    Serial.print(" ");
-//    Serial.print(hour());
-//    printDigits(minute());
-//    printDigits(second());
-//    Serial.print(" ");
-//    Serial.print(day());
-//    Serial.print(" ");
-//    Serial.print(month());
-//    Serial.print(" ");
-//    Serial.print(year());
-//    Serial.println();
-    Serial.print(millis());
+    Serial.print(steps);
+    Serial.print(".");
+    Serial.print(hour());
+    printDigits(minute());
+    printDigits(second());
+    Serial.print(" ");
+    Serial.print(day());
+    Serial.print("/");
+    Serial.print(month());
+    Serial.print("/");
+    Serial.print(year());
     Serial.print("\n");
     saveData = false;
     delay(500);
@@ -66,25 +85,43 @@ void loop() {
 }
 
 void digitalClockDisplay(){
-//Serial.print(hour());
-//printDigits(minute());
-//printDigits(second());
-//Serial.print(" ");
-//Serial.print(day());
-//Serial.print(" ");
-//Serial.print(month());
-//Serial.print(" ");
-//Serial.print(year());
-//Serial.println();
-Serial.print(millis());
-Serial.print("\n");
+  // digital clock display of the time
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(" ");
+  Serial.print(month());
+  Serial.print(" ");
+  Serial.print(year()); 
+  Serial.println(); 
 }
 
 void printDigits(int digits){
-Serial.print(":");
-if(digits < 10){
-  Serial.print('0');
+  // utility function for digital clock display: prints preceding colon and leading 0
+  Serial.print(":");
+  if(digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
+}
+
+
+void processSyncMessage() {
+  unsigned long pctime;
+  const unsigned long DEFAULT_TIME = 1357041600; // Jan 1 2013
+
+  if(Serial.find(TIME_HEADER)) {
+     pctime = Serial.parseInt();
+     if( pctime >= DEFAULT_TIME) { // check the integer is a valid time (greater than Jan 1 2013)
+       setTime(pctime); // Sync Arduino clock to the time received on the serial port
+     }
   }
-Serial.print(digits);
+}
+
+time_t requestSync()
+{
+  Serial.print(TIME_REQUEST);  
+  return 0; // the time will be sent later in response to serial msg
 }
 
--- StepCounter/WriteToTextFile/WriteToTextFile.pde
+++ StepCounter/WriteToTextFile/WriteToTextFile.pde
@@ -1,11 +1,39 @@
 import processing.serial.*;
 import java.io.*;
+import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 Serial mySerial;
 PrintWriter output;
+public static final String TIME_HEADER = "T";
+public static final char TIME_REQUEST = 'z';
+public static final char LF = 10;
+public static final char CR = 13;
+boolean timeRequest = false;
 
 void setup() {
   mySerial = new Serial(this, Serial.list()[0], 9600);
-  
+  while (timeRequest == false){
+    if(mySerial.available()>0){
+      char val = char(mySerial.read());
+      if (val == TIME_REQUEST){
+        long t = getTimeNow();
+        sendTimeMessage(TIME_HEADER, t);
+        timeRequest = true;
+      }
+      else{
+        if (val == LF)
+          ;
+        else if (val == CR)
+          println();
+        else
+          print(val);
+      }
+    }
+    delay(500);
+  }
+  mySerial.clear();  
   File data = new File("C:\\Users\\Tony Ren\\Documents\\421_final_project\\StepCounter\\data.txt");
   try{
   if(!data.exists()){
@@ -30,4 +58,21 @@ void keyPressed() {
   output.flush();
   output.close();
   exit();
+}
+
+void sendTimeMessage(String header, long time) {  
+  String timeStr = String.valueOf(time);  
+  mySerial.write(header);  // send header and time to arduino
+  mySerial.write(timeStr); 
+  mySerial.write('\n');  
+}
+
+long getTimeNow(){
+  // java time is in ms, we want secs    
+  Date d = new Date();
+  Calendar cal = new GregorianCalendar();
+  long current = d.getTime()/1000;
+  long timezone = cal.get(cal.ZONE_OFFSET)/1000;
+  long daylight = cal.get(cal.DST_OFFSET)/1000;
+  return current + timezone + daylight; 
 }
